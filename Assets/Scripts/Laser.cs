771c741eeafc5c7061d7091816d7ac74d763d3d5
Rett-Lindago <bradburyoliver@yahoo.com>
2018-04-18 12:22:38 -0700

    ok heres the files
    have fun shitlord

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+ public float damage = 10f, range = 100f;
+
+public GameObject bluelaser;
+	void firing () {
+		var laserAncestor = GameObject.Find("bluelaser");
+		var newLaser = GameObject.Instantiate(laserAncestor,{
+
+			Vector2 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+ 
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+            );
+        transform.up = direction;
+		} );
+
+		
+
+	}
+}
---
+++ Assets/Scripts/playerscript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class playerscript : MonoBehaviour {
+ 
+    public float movementspeed, movementspeedmodifier, currenthealth, maxhealth = 100, currentarmor = 0, maxarmor = 100;
+    public bool moving = false;
+
+    private Rigidbody2D rb2d;
+    private CharacterController charcont;
+ 
+    // Use this for initialization
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        charcont = GetComponent<CharacterController>();
+ 
+        currenthealth = maxhealth;
+        maxarmor = currentarmor;
+ 
+    }
+ 
+    // Update is called once per frame
+    void Update ()
+    {
+        facemouse();
+    }
+ 
+    //deals with rigidbodys only so movement basically
+    void FixedUpdate()
+    {
+		moving = false;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.AddForce(Vector2.up * movementspeed * movementspeedmodifier);
+            moving = true;
+        }
+ 
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.AddForce(Vector2.down * movementspeed * movementspeedmodifier);
+            moving = true;
+        }
+ 
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForce(Vector2.left * movementspeed * movementspeedmodifier);
+            moving = true;
+        }
+ 
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForce(Vector2.right * movementspeed * movementspeedmodifier);
+            moving = true;
+        }
+		
+
+		if(!moving) rb2d.drag = 5;
+ 
+    }
+ 
+    void facemouse()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+ 
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+            );
+        transform.up = direction;
+    }
+}
